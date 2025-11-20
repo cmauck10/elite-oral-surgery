db138bccaab1eb5429d2dc37c409a161689c0f70
Chris Mauck <chrismauck10@gmail.com>
2025-11-19 23:37:15 -0500

    Deploy static site

--- _next/static/wOZGk3AEUB0HldJIYGa4F/_buildManifest.js
+++ _next/static/wOZGk3AEUB0HldJIYGa4F/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/elite-oral-surgery//_next/:path+",
-        "destination": "/elite-oral-surgery/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
